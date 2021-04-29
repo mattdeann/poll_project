5be6a7b4a2d8e7d2d0e063ff5f87ad0243d8970a
mattdean <matthewallandean@gmail.com>
2021-04-29 14:51:04 -0600

    Edit the index view

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # Get latest questions by pub date. Colon is to return only 5
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # Join the questions by commas
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at the question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
