b9a01d48643c5688657846a7539c1d9a079d6b0e
mattdean <matthewallandean@gmail.com>
2021-04-30 09:28:08 -0600

    Connect CSS static file

--- polls/tests.py
+++ polls/tests.py
@@ -6,7 +6,14 @@ from django.urls import reverse
 
 from polls.models import Question
 
-# Create your tests here.
+# Helper Function
+
+def create_question(question_text, days):
+
+  time = timezone.now() + datetime.timedelta(days=days)
+  return Question.objects.create(question_text=question_text, pub_date=time)
+
+# Tests
 
 class QuestionModelTests(TestCase):
   def test_was_published_recently_with_old_question(self):
@@ -27,11 +34,6 @@ class QuestionModelTests(TestCase):
     time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
     recent_question = Question(pub_date=time)
     self.assertIs(recent_question.was_published_recently(), True)
-    
-def create_question(question_text, days):
-  
-  time = timezone.now() + datetime.timedelta(days=days)
-  return Question.objects.create(question_text=question_text, pub_date=time)
 
 class QuestionIndexViewTests(TestCase):
   def test_no_questions(self):
--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
-ffrom django.utils import timezone
+from django.utils import timezone
 
 from .models import Choice, Question
 
